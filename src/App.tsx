2908e2e73349aa894fde6790ca331442dc9d0d59
weebNeedWeed <rivenmle@gmail.com>
2024-08-30 00:08:40 +0700

    Update authorization

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,10 @@ function App() {
 
   return (
     <Authenticator>
-      {({ signOut }) => (
+      {({ signOut, user }) => (
         <main>
           <h1>My todos</h1>
+          <h1>{user?.signInDetails?.loginId}'s Todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
